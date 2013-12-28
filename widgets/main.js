8f6a3b90bbb801b53a3cb8e6e306d6386b9fc411
hepeguo <hepeguo@gmail.com>
2013-12-28 22:36:51 +0800

    add tpl, add ext lan set

--- widgets/main.js
+++ widgets/main.js
@@ -2,10 +2,17 @@ var fs = require('fs');
 var path = require('path');
 var argv = require('optimist').
 default ({
-	ext: "json",
+	ext: "md",
 	path: "./controls/",
 	lan: "en"
 }).argv;
+var dot = require('dot');
+var tpl = require("./tpl.js").tpl();
+
+// var foo = {
+// 	foo: "guozy"
+// };
+// var doc = dot.template(tpl)(foo);
 
 var config = {
 	ext: argv.ext,
@@ -13,12 +20,60 @@ var config = {
 	lan: argv.lan
 };
 
-var getFile = function(path) {
+var data = [],
+	lanFile;
+var controls = getFile(path.join(config.path, "controls.json"));
+
+start();
+
+
+function start() {
+	if (config.lan == "en") {
+		if (config.ext == "html" || config.ext == "MD" || config.ext == "md") {
+			getData(config.ext, "en");
+		} else if (config.ext == "both") {
+			getData("html", "en");
+			getData("md", "en");
+		} else {
+			console.log("ext must be 'md' or 'html' or 'both'!")
+		}
+
+	} else if (config.lan == "zh") {
+		//getData(config.ext, "zh");
+		if (config.ext == "html" || config.ext == "MD" || config.ext == "md") {
+			getData(config.ext, "zh");
+		} else if (config.ext == "both") {
+			getData("html", "zh");
+			getData("md", "zh");
+		} else {
+			console.log("ext must be 'md' or 'html' or 'both'!")
+		}
+	} else if (config.lan == "both") {
+		//getData(config.ext, "zh");
+		//getData(config.ext, "en");
+		if (config.ext == "html" || config.ext == "MD" || config.ext == "md") {
+			getData(config.ext, "en");
+			getData(config.ext, "zh");
+		} else if (config.ext == "both") {
+			getData("html", "en");
+			getData("md", "en");
+			getData("html", "zh");
+			getData("md", "zh");
+		} else {
+			console.log("ext must be 'md' or 'html' or 'both'!")
+		}
+	} else {
+		console.log("lan must be 'en' or 'zh' or 'both'!");
+	}
+};
+
+function getFile(path) {
 	var data = fs.readFileSync(path, 'utf8');
 	data = JSON.parse(data);
 	return data;
 };
-var i18n = function(object, file) {
+
+function i18n(object, file) {
 	for (var key in object) {
 		var value = object[key] ? object[key] : "";
 		if (typeof value == "object") {
@@ -34,51 +89,52 @@ var i18n = function(object, file) {
 			var length = keys.length;
 			switch (length) {
 				case 1:
-					object[key] = en[file][keys[0]];
+					object[key] = lanFile[file][keys[0]];
 					break;
 				case 2:
-					object[key] = en[file][keys[0]][keys[1]];
+					object[key] = lanFile[file][keys[0]][keys[1]];
 					break;
 				case 3:
-					object[key] = en[file][keys[0]][keys[1]][keys[2]];
+					object[key] = lanFile[file][keys[0]][keys[1]][keys[2]];
 					break;
 				case 4:
-					object[key] = en[file][keys[0]][keys[1]][keys[2]][keys[3]];
+					object[key] = lanFile[file][keys[0]][keys[1]][keys[2]][keys[3]];
 			}
 		}
 	}
 };
 
-var dataEn = [],
-	dataCh = [],
-	en, zh;
+function getData(ext, lan) {
+	data.length = 0;
+	controls.forEach(function(item, index) {
+		var package = getFile(path.join(config.path, item, "/package.json"));
+		var signal, slot, option;
+		if (package.options) option = getFile(path.join(config.path, item, package.options));
+		if (package.signals) signal = getFile(path.join(config.path, item, package.signals));
+		if (package.slots) slot = getFile(path.join(config.path, item, package.slots));
+		var control = {
+			name: item,
+			package: package,
+			options: option,
+			signals: signal,
+			slots: slot
+		};
+		if (lan == "en") {
+			lanFile = getFile(path.join(config.path, item, "_locales/en-US/package.json"));
+		} else if (lan == "zh") {
+			lanFile = getFile(path.join(config.path, item, "_locales/zh-CN/package.json"));
+		}
 
-var controls = getFile(path.join(config.path, "controls.json"));
-controls.forEach(function(item, index) {
-	var package = getFile(path.join(config.path, item, "/package.json"));
-	var signal, slot, option;
-	if (package.options) option = getFile(path.join(config.path, item, package.options));
-	if (package.signals) signal = getFile(path.join(config.path, item, package.signals));
-	if (package.slots) slot = getFile(path.join(config.path, item, package.slots));
-	var control = {
-		name: item,
-		package: package,
-		options: option,
-		signals: signal,
-		slots: slot
-	};
-	en = getFile(path.join(config.path, item, "_locales/en-US/package.json"));
-	zh = getFile(path.join(config.path, item, "_locales/zh-CN/package.json"));
+		i18n(control.package, "package");
+		i18n(control.options, "options");
+		if (control.signals) i18n(control.signals, "signals");
+		if (control.slots) i18n(control.slots, "slots");
 
-	i18n(control.package, "package");
-	i18n(control.options, "options");
-	if (control.signals) i18n(control.signals, "signals");
-	if (control.slots) i18n(control.slots, "slots");
+		data.push(control);
+	});
 
-	dataEn.push(control);
-});
+	fs.writeFileSync("doc-" + lan + "." + ext, JSON.stringify(data, null, 4));
+};
 
-
-console.log(dataEn);
-
-//console.log(config);
+//fs.writeFileSync("doc.json", JSON.stringify(data, null, 4));
+//fs.writeFileSync("doc.html", doc);
---
+++ widgets/tpl.js
@@ -0,0 +1,3 @@
+exports.tpl = function () {
+	return '<h1>Here is a sample template {{=it.foo}}</h1>';
+};
