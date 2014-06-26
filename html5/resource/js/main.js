2acc283b04bdc61e8dbcd125c320fed4aab1e1f7
hepeguo <hepeguo@gmail.com>
2014-06-26 18:06:30 +0800

    add recongnition

--- html5/resource/js/main.js
+++ html5/resource/js/main.js
@@ -11,6 +11,7 @@
 	var $video = $("video");
 	var $videoSrc = $("videoSrc");
 	var $recognize = $("recognize");
+	var $textarea = $("textarea");
 
 	//发音
 	$speak.onclick = function() {
@@ -38,9 +39,9 @@
 		speechSynthesis.speak(msg);
 
 		//输出语气列表
-		win.speechSynthesis.getVoices().forEach(function(voice) {
-			console.log(voice.name, voice.default ? '(default)' : '');
-		});
+		// win.speechSynthesis.getVoices().forEach(function(voice) {
+		// 	console.log(voice.name, voice.default ? '(default)' : '');
+		// });
 	};
 
 	//视频
@@ -63,7 +64,9 @@
 						minHeight: 720,
 						minFrameRate: 30
 					},
-					optional: [{minFrameRate: 60}]
+					optional: [{
+						minFrameRate: 60
+					}]
 				},
 				audio: true
 			}, function(stream) {
@@ -90,7 +93,121 @@
 	}
 
 	//语言识别
+	var final_transcript = "";
+	//支持的语言类型
+	var langs =
+		[
+			['Afrikaans', ['af-ZA']],
+			['Bahasa Indonesia', ['id-ID']],
+			['Bahasa Melayu', ['ms-MY']],
+			['Català', ['ca-ES']],
+			['Čeština', ['cs-CZ']],
+			['Deutsch', ['de-DE']],
+			['English', ['en-AU', 'Australia'],
+				['en-CA', 'Canada'],
+				['en-IN', 'India'],
+				['en-NZ', 'New Zealand'],
+				['en-ZA', 'South Africa'],
+				['en-GB', 'United Kingdom'],
+				['en-US', 'United States']
+			],
+			['Español', ['es-AR', 'Argentina'],
+				['es-BO', 'Bolivia'],
+				['es-CL', 'Chile'],
+				['es-CO', 'Colombia'],
+				['es-CR', 'Costa Rica'],
+				['es-EC', 'Ecuador'],
+				['es-SV', 'El Salvador'],
+				['es-ES', 'España'],
+				['es-US', 'Estados Unidos'],
+				['es-GT', 'Guatemala'],
+				['es-HN', 'Honduras'],
+				['es-MX', 'México'],
+				['es-NI', 'Nicaragua'],
+				['es-PA', 'Panamá'],
+				['es-PY', 'Paraguay'],
+				['es-PE', 'Perú'],
+				['es-PR', 'Puerto Rico'],
+				['es-DO', 'República Dominicana'],
+				['es-UY', 'Uruguay'],
+				['es-VE', 'Venezuela']
+			],
+			['Euskara', ['eu-ES']],
+			['Français', ['fr-FR']],
+			['Galego', ['gl-ES']],
+			['Hrvatski', ['hr_HR']],
+			['IsiZulu', ['zu-ZA']],
+			['Íslenska', ['is-IS']],
+			['Italiano', ['it-IT', 'Italia'],
+				['it-CH', 'Svizzera']
+			],
+			['Magyar', ['hu-HU']],
+			['Nederlands', ['nl-NL']],
+			['Norsk bokmål', ['nb-NO']],
+			['Polski', ['pl-PL']],
+			['Português', ['pt-BR', 'Brasil'],
+				['pt-PT', 'Portugal']
+			],
+			['Română', ['ro-RO']],
+			['Slovenčina', ['sk-SK']],
+			['Suomi', ['fi-FI']],
+			['Svenska', ['sv-SE']],
+			['Türkçe', ['tr-TR']],
+			['български', ['bg-BG']],
+			['Pусский', ['ru-RU']],
+			['Српски', ['sr-RS']],
+			['한국어', ['ko-KR']],
+			['中文', ['cmn-Hans-CN', '普通话 (中国大陆)'],
+				['cmn-Hans-HK', '普通话 (香港)'],
+				['cmn-Hant-TW', '中文 (台灣)'],
+				['yue-Hant-HK', '粵語 (香港)']
+			],
+			['日本語', ['ja-JP']],
+			['Lingua latīna', ['la']]
+		];
+		//初始化speechRecognition
+	if ("webkitSpeechRecognition" in win) {
+		var recognition = new webkitSpeechRecognition();
+		recognition.continuous = true;
+		recognition.interimResults = true;
+
+		recognition.onstart = function() {
+			console.log("start");
+		}
+		recognition.onresult = function(event) {
+			var interim_transcript = '';
+			for (var i = event.resultIndex; i < event.results.length; ++i) {
+				if (event.results[i].isFinal) {
+					final_transcript += event.results[i][0].transcript;
+					if(event.results[i][0].transcript == "清空" || event.results[i][0].transcript == "清除") {
+						final_transcript = " ";
+					}
+				} else {
+					console.log(event.results[i][0].transcript);
+				}
+			}
+			$textarea.value = final_transcript;
+		}
+		recognition.onerror = function(event) {
+			console.log(event);
+		}
+		recognition.onend = function() {
+			console.log("end");
+		}
+	}
+	//设置语言类型
+	recognition.lang = 'cmn-Hans-CN';
+	//开始语音识别
+	var isStart = false;
 	$recognize.onclick = function() {
-		console.log("recognize!!!!!!!!!!!!!!!!!!!!!");
+		if (recognition) {
+			if (!isStart) {
+				recognition.start();
+				isStart = true;
+			} else {
+				recognition.stop();
+				isStart = false;
+			}
+		}
 	}
 })(window, document);
